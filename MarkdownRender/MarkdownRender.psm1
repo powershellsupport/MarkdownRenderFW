0ad8c3d5e623d68aab4a43781229e957eea7def3
David Sass <elethulyelenseg@hotmail.com>
2021-09-19 22:09:06 +0200

    feat(MarkdownRender): Windows PowerShell Module

---
+++ MarkdownRender/MarkdownRender.psm1
@@ -0,0 +1,33 @@
+function ConvertFrom-Markdown {
+    [CmdletBinding()]
+    param (
+        # Specifies a path to one or more locations.
+        [Parameter(Mandatory=$true,
+                   Position=0,
+                   ParameterSetName="Path",
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   HelpMessage="Path to one or more locations.")]
+        [Alias("PSPath")]
+        [ValidateNotNullOrEmpty()]
+        [string[]]
+        $Path
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        foreach ($onePath in $Path) {
+            $content = Get-Content -Path $onePath -Raw
+            [MarkdownRender.MarkdownConverter]::Convert($content,[MarkdownRender.MarkdownConversionType]::HTML,[MarkdownRender.PSMarkdownOptionInfo]::new()) #| Select-Object -ExpandProperty Html
+            #[MarkdownRender.MarkdownConverter]::Convert($(Get-Content C:\source\github\MarkdownRenderFW\README.md),[MarkdownRender.MarkdownConversionType]::HTML,[MarkdownRender.PSMarkdownOptionInfo]::new())
+        }
+        
+    }
+    
+    end {
+        
+    }
+}
